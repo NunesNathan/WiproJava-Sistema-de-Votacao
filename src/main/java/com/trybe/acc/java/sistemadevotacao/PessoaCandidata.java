f2fb528bdb2ca1fd12b4d6f2d3875fa96a1e7ca4
NunesNathan <nunesxd7@hotmail.com>
2022-08-01 19:50:00 -0300

    feat: pessoa classes

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,41 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  private int numero;
+  private int votos = 0;
 
+  public PessoaCandidata(String nome, int numero) {
+    super(nome);
+    this.numero = numero;
+  }
+
+  @Override
+  public String getNome() {
+    return this.nome;
+  }
+
+  @Override
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
 }
